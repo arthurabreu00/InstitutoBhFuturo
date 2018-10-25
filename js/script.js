8c7cdc15baafcc1e9ee696edf6549948c310a1e5
arthurabreu00 <arthurabreu00@gmail.com>
2018-10-25 16:54:47 -0300

    Jquery teste

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// jQuery(function () {
+//     jQuery(window).scroll(()=> {
+//      if (jQuery(this).scrollTop() > 400) {
+//       $("nav").css('background', 'rgba(30,156,161,1)')
+//       $("nav").css('display', 'fixed');
+//      } else {
+//       $("nav").css('display','block');
+//       $("nav").css('background', 'none');
+//      }
+//     });
+//    });
