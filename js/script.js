21ae1309455f0e018efa5447c33c5aeb76eab21e
Arthur Flávio de Abreu Tavares <277093@UTRAMIGBH.SEDE>
2018-11-09 22:25:01 -0200

    mais testes de contato

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
+$('.form-contato').validator();
 
-
-document.querySelector('.form-submit').addEventListener('submit',btn=>{
-    btn.preventDefault();
-
-    document.querySelectorAll('.form-control').forEach(e=>{
-        document.querySelectorAll('.form-group').forEach(f => {
-            if(e.value == ''){
-                f.innerHTML += `<div class="alert alert-danger" role="alert">
-                Um simples alerta danger. Olha só!
-              </div>`
-            };
-        });
-
-    });
-    
-})
+$('.form-submit').click(event => {
+    event.preventDefault();
+});
