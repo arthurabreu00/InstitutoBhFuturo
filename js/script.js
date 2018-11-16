5402f39fdc8a3b04527d92dcfc20c352633caf62
Makense <makense22@gmail.com>
2018-11-16 17:55:37 -0200

    Correção apoiadores

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
 document.querySelectorAll('.figure-img').forEach(figura =>{
     
-
-    // figura.addEventListener('touchmove',()=>{
-    //     let caminho = figura.src;
-    //     figura.src = caminho.replace('-cinza',"");
-    // })
-
-    // figura.addEventListener('touchend',()=>{
-    //     let caminho = figura.src;
-    //     figura.src = caminho.replace('.png','-cinza.png')
-    // })
+    let caminho = figura.src;
 
     figura.addEventListener('mouseover',()=>{
-        let caminho = figura.src;
+        caminho = figura.src;
         figura.src = caminho.replace('-cinza',"");
     });
 
     figura.addEventListener('mouseout',()=>{
-        let caminho = figura.src;
+        caminho = figura.src;
         figura.src = caminho.replace('.png','-cinza.png')
 
     });
