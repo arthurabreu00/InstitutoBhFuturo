99a7d2f69f9a2e30d4b89bb44a3981df0d465c26
Arthur Tavares <arthurabreu00@gmail.com>
2018-11-18 16:46:26 -0200

    ScrollReveal

---
+++ js/ScrollReveal.js
@@ -0,0 +1,56 @@
+window.sr = ScrollReveal();
+sr.reveal('.carousel-caption', {
+    duration: 2000,
+    origin:'top',
+    distance:'300px'
+});
+sr.reveal('#unesco', {
+    duration: 2300,
+    origin:'left',
+    distance:'300px'
+});
+sr.reveal('#senai', {
+    duration: 2600,
+    origin:'left',
+    distance:'300px'
+});
+sr.reveal('#fda', {
+    duration: 2900,
+    origin:'left',
+    distance:'300px',
+});
+sr.reveal('#cemig', {
+    duration: 3100,
+    origin:'left',
+    distance:'300px',
+});
+sr.reveal('#amipao', {
+    duration: 3400,
+    origin:'left',
+    distance:'300px'
+});
+sr.reveal('#mg', {
+    duration: 3700,
+    origin:'left',
+    distance:'300px'
+});
+sr.reveal('#puc', {
+    duration: 4000,
+    origin:'left',
+    distance:'300px'
+});
+sr.reveal('#globo', {
+    duration: 4300,
+    origin:'left',
+    distance:'300px'
+});
+sr.reveal('#fmb', {
+    duration: 4600,
+    origin:'left',
+    distance:'300px'
+});
+sr.reveal('#mrv', {
+    duration: 4900,
+    origin:'left',
+    distance:'300px'
+});
