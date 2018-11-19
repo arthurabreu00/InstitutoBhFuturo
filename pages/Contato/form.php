53f41a739c491909e3c8f61edce66f0b502f0373
Arthur Tavares <arthurabreu00@gmail.com>
2018-11-19 01:01:45 -0200

    Formulario PHP quebrado

--- pages/Contato/form.php
+++ pages/Contato/form.php
@@ -1,8 +1,38 @@
 <?php
 
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$assunto = $_POST['assunto'];
-$mensagem = $_POST['mensagem'];
+function pegaValor($valor) {
+    return isset($_POST[$valor]) ? $_POST[$valor] : '';
+}
+
+function validaEmail($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function enviaEmail($de, $assunto, $mensagem, $para, $email_servidor) {
+    $headers = "From: $email_servidor\r\n" .
+               "Reply-To: $de\r\n" .
+               "X-Mailer: PHP/" . phpversion() . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+  
+  mail($para, $assunto, nl2br($mensagem), $headers);
+}
+
+$email_servidor = "utramiginformatica42@gmail.com";
+$para = "utramiginformatica42@gmail.com";
+$de = pegaValor("email");
+$mensagem = pegaValor("mensagem");
+$assunto = pegaValor("assunto");
+
+
+if ($nome && validaEmail($de) && $mensagem) {
+    enviaEmail($de, $assunto, $mensagem, $para, $email_servidor);
+    $pagina = "contato.html";
+    echo 'Sucesso';
+} else {
+    $pagina = "contato.html";
+}
+
+header("location:$pagina");
 
 ?>
