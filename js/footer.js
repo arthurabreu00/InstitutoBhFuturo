8703d12182231f4c89aaf2720a63d22b0ee80b1b
arthurabreu00 <arthurabreu00@gmail.com>
2018-11-23 16:07:41 -0200

    Atualizando no footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,29 +1,84 @@
-document.querySelector('footer').innerHTML += `
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.6429147354283!2d-43.914320684494164!3d-19.939446986598472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa699a250148705%3A0xef566cafb0a6ce74!2sR.+Des.+Mario+Matos%2C+564+-+Serra%2C+Belo+Horizonte+-+MG%2C+30220-410!5e0!3m2!1spt-BR!2sbr!4v1528830642861"
-      height="400" frameborder="0" style="border:0"  class="maps" allowfullscreen=""></iframe>
+// document.querySelector('footer').innerHTML += `
+// <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.6429147354283!2d-43.914320684494164!3d-19.939446986598472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa699a250148705%3A0xef566cafb0a6ce74!2sR.+Des.+Mario+Matos%2C+564+-+Serra%2C+Belo+Horizonte+-+MG%2C+30220-410!5e0!3m2!1spt-BR!2sbr!4v1528830642861"
+//       height="400" frameborder="0" style="border:0"  class="maps" allowfullscreen=""></iframe>
 
-    <section class="info-rodape">
-      <div class="container-fluid p-3 p-md-3">
-        <ul class="bd-footer-links social">
-          <li><a href="../index.html"> <img src="../img/icon.png"></a> </li>
-          <li><a href="historia.html">IBHF</a></li>
-          <li><a href="contato.html">Contato</a></li>
-          <li><a href="#">Oficinas</a></li>
-          <li><a href="#">Doações</a></li>
-          <div id="midias-sociais">
-            <li><a href="https://twitter.com/bhfuturo" target="_blank"> <i class="fab fa-twitter"></i> </a></li>
-            <li><a href="https://facebook.com/institutobhfuturo" target="_blank"> <i class="fab fa-facebook"></i> </a></li>
-            <li><a href="https://instagram.com/bhfuturo" target="_blank"> <i class="fab fa-instagram"></i> </a></li>
-          </div>
-        </ul>
-      </div>
-    </section>
+    // <section class="info-rodape">
+    //   <div class="container-fluid p-3 p-md-3">
+    //     <ul class="bd-footer-links social">
+    //       <li><a href="../index.html"> <img src="../img/icon.png"></a> </li>
+    //       <li><a href="historia.html">IBHF</a></li>
+    //       <li><a href="contato.html">Contato</a></li>
+    //       <li><a href="#">Oficinas</a></li>
+    //       <li><a href="#">Doações</a></li>
+    //       <div id="midias-sociais">
+    //         <li><a href="https://twitter.com/bhfuturo" target="_blank"> <i class="fab fa-twitter"></i> </a></li>
+    //         <li><a href="https://facebook.com/institutobhfuturo" target="_blank"> <i class="fab fa-facebook"></i> </a></li>
+    //         <li><a href="https://instagram.com/bhfuturo" target="_blank"> <i class="fab fa-instagram"></i> </a></li>
+    //       </div>
+    //     </ul>
+    //   </div>
+    // </section>
 
-    <section class="info-rodape-extra">
-      <div class="container-fluid" align="center">
-        <a href=""> UTRAMIG @ - Todos os direitos reservados </a> -
-        Av. Afonso Pena, 3400. Cruzeiro. 30130-009. Belo Horizonte. MG.
-      </div>
-    </section>
+//     <section class="info-rodape-extra">
+//       <div class="container-fluid" align="center">
+//         <a href=""> UTRAMIG @ - Todos os direitos reservados </a> -
+//         Av. Afonso Pena, 3400. Cruzeiro. 30130-009. Belo Horizonte. MG.
+//       </div>
+//     </section>
 
-`;
+// `;
+
+document.querySelector('footer').innerHTML += 
+` <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.6429147354283!2d-43.914320684494164!3d-19.939446986598472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa699a250148705%3A0xef566cafb0a6ce74!2sR.+Des.+Mario+Matos%2C+564+-+Serra%2C+Belo+Horizonte+-+MG%2C+30220-410!5e0!3m2!1spt-BR!2sbr!4v1528830642861"
+height="400" frameborder="0" style="border:0"  class="maps" allowfullscreen=""></iframe>
+
+<section class="info-rodape">
+<div class="container-fluid p-3 p-md-3">
+  <ul class="bd-footer-links social">
+    <li><a href="index.html"> <img src="../img/icon.png"></a> </li>
+    <li><a href="historia.html">IBHF</a></li>
+    <li><a href="contato.html">Contato</a></li>
+    <li><a href="oficina.html">Oficinas</a></li>
+    <li><a href="doe.html">Doações</a></li>
+    
+    <div id="midias-sociais">
+      <li><a href="https://twitter.com/bhfuturo" target="_blank"> <i class="fab fa-twitter"></i> </a></li>
+      <li><a href="https://facebook.com/institutobhfuturo" target="_blank"> <i class="fab fa-facebook"></i> </a></li>
+      <li><a href="https://instagram.com/bhfuturo" target="_blank"> <i class="fab fa-instagram"></i> </a></li>
+    </div>
+  </ul>
+</div>
+</section>
+
+<section class="info-rodape-extra text-white">
+<hr>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-4 destaque">
+        <i class="fa fa-phone" id="phone"></i>  31 3259-4015 / 313259-3885
+    </div>
+    <div class="col-lg-3 destaque">
+        <i class="far fa-envelope" id="envelope"></i> contato@institutobhfuturo.org.br
+    </div>
+    <div class="col-lg-5 destaque">
+        <i class="fas fa-map-marker-alt" id="marker"></i>  Rua Desembargador Mário Matos, 578, Serra - Belo Horizonte/MG
+    </div>
+  </div>
+</div>
+</section> 
+
+<section class=" info-rodape-extra bg-primary text-white">
+  
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12 text-center pt-10">
+         <a href="http://utramig.mg.gov.br/" class="text-white text-decoration-none"> Utramig © - Fundação de Educação para o Trabalho de Minas Gerais </a> 
+         <br>
+         
+    </div>
+  </div>
+</section> 
+
+
+</div>
+</div> `
